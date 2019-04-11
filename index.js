14a3b97a367c11214133b87947bdf3d38fadcada
Esfahani <esfahanimichael@gmail.com>
2019-04-10 18:49:45 -0700

    got my test api point to function

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const PORT = process.env.PORT || 9000;
+const ENV = process.env.NODE_ENV || 'development';
+
+
+const app = express();
+
+app.use(cors());
+
+app.get('/api/test', (req, res) => {
+
+    res.send('<h1>API Status</h1>')
+});
+
+app.listen(PORT, () => {
+    console.log('Server Running at localhost:', + PORT);
+}).on('error', (err => {
+    console.log("Server listen error, you probably already have a server on PORT:" + PORT);
+}))
