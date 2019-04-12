4d297b7db10b53e27c43a31bfcbe005925046ac9
Esfahani <esfahanimichael@gmail.com>
2019-04-12 14:47:26 -0700

    made a test component for the front end

---
+++ client/src/components/test.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    render(){
+        return(
+            <h1>this is the test comp</h1>
+        )
+    }
+}
+
+export default Test;
