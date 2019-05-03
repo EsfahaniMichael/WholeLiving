eb9047a777b461697c3057f52d437597ffce3b5e
Esfahani <esfahanimichael@gmail.com>
2019-05-03 15:30:20 -0700

    added in react router dom, added routes

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -4,16 +4,17 @@ import 'materialize-css/dist/js/materialize.min';
 import '../assets/css/app.scss';
 // import AllLocations from './mapping/allLocations';
 import Map from './mapping/map';
+import {Route, Switch} from 'react-router-dom';
 
 
-const App = () => (
-    <div>
-        <div className="container">
-            <h1 className="center">Whole LIVING!</h1>
-            {/* <AllLocations/> */}
-            <Map />
-        </div>
-    </div>
-);
+
+function App(){
+    return(
+        <Switch>
+            <Route path="/" component = {Map}/>
+        </Switch>
+    );
+
+}
 
 export default App;
